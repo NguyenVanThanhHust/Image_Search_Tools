9237fac9c0ce3f14c92941f61ccb32e7c5861766
Nguyen Van Thanh <NguyenVanThanhHust@gmail.com>
2020-02-08 10:22:29 +0700

    refactor source code

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ from torchvision import datasets
 
 from lshash import LSHash
 from conf import settings
-from utils_ai import build_network
+from utils_ai import build_network, get_feature_single_img, create_feature
 from utils import get_class_name_from_string
 
 logger = logging.getLogger(__name__)
@@ -50,7 +50,7 @@ def get_similar_item_image(imgpath, n_items=3):
     global net
     global feature_dict
     global lsh
-    feature = get_feature_single_img(imgpath)
+    feature = get_feature_single_img(net, imgpath)
     print("image name: ", imgpath)
     response = lsh.query(feature, 
                      num_results=n_items+1, distance_func='l1norm')
@@ -62,67 +62,9 @@ def get_similar_item_image(imgpath, n_items=3):
     labels = [label_1, label_2, label_3]
     return labels, paths
 
-def get_feature_single_img(image_path):
-    global net
-    """
-    """
-    device = torch.device("cuda")
-    image_transforms =  transforms.Compose([
-                        transforms.Resize((112, 112)),
-                        transforms.ToTensor(),
-                        transforms.Normalize(settings.CIFAR100_TRAIN_MEAN, 
-                                             settings.CIFAR100_TRAIN_STD)])
-
-    img = Image.open(image_path)
-    img_tensor = image_transforms(img)
-    model = net
-    if torch.cuda.is_available():
-        img_tensor = img_tensor.view(1, 3, 112, 112).cuda()
-    else:
-        img_tensor = img_tensor.view(1, 3, 112, 112)
-    with torch.no_grad():
-        model.eval()
-        # model ouputs log probabilities
-        out = model(img_tensor)  # <class 'torch.Tensor'>  torch.Size([1, 58])
-        ps = torch.exp(out) #  <class 'torch.Tensor'> torch.Size([1, 58])
-        feature = ps.cpu().numpy()[0]
-    return feature
-
-def predict_author_single_img(model, image_path):
-    """
-    
-    """
-    device = torch.device("cuda")
-    image_transforms =  transforms.Compose([
-                        transforms.Resize((112, 112)),
-                        transforms.ToTensor(),
-                        transforms.Normalize(settings.CIFAR100_TRAIN_MEAN, 
-                                             settings.CIFAR100_TRAIN_STD)])
-
-    img = Image.open(image_path)
-    img_tensor = image_transforms(img)
-
-    if torch.cuda.is_available():
-        img_tensor = img_tensor.view(1, 3, 112, 112).cuda()
-    else:
-        img_tensor = img_tensor.view(1, 3, 112, 112)
-    with torch.no_grad():
-        model.eval()
-        # model ouputs log probabilities
-        out = model(img_tensor)  # <class 'torch.Tensor'>  torch.Size([1, 58])
-        ps = torch.exp(out) #  <class 'torch.Tensor'> torch.Size([1, 58])
-        feature = ps.cpu().numpy()[0]
-
-        topk, topclass = ps.topk(3, dim=1)
-        
-        sum_topk = int(topk.cpu().numpy()[0][0]) + int(topk.cpu().numpy()[0][1]) + int(topk.cpu().numpy()[0][2])
-    return idx_to_class[topclass.cpu().numpy()[0][0]], (topk.cpu().numpy()[0][0])/sum_topk, feature
-
-
 def allowed_file(filename):
         return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 @app.route("/", methods=['GET', 'POST'])
 def upload_file():
@@ -195,31 +137,6 @@ def predict():
                            img_1=path_1, img_2 = path_2, img_3 = path_3, 
                            labels_1 = labels[0], labels_2 = labels[1], labels_3 = labels[2])
 
-def create_feature(list_author, net):
-    global example_image_dir
-    list_feature = list()
-    image_paths = list()
-    ## Locality Sensitive Hashing
-    k = 10 # hash size
-    L = 5  # number of tables
-    d = 58 # Dimension of Feature vector
-    lsh = LSHash(hash_size=k, input_dim=d, num_hashtables=L)
-    for subfolder in list_author.keys():
-        subfolder_path = os.path.join(example_image_dir, subfolder)
-        count_items = len([name for name in os.listdir(subfolder_path) if os.path.isfile(os.path.join(subfolder_path, name))])
-        # print(subfolder)
-        sum_acc = 0
-        sum_confiden = 0
-
-        for img in os.listdir(subfolder_path):
-            image_path = os.path.join(subfolder_path, img)
-            author, confidence, feature = predict_author_single_img(net, image_path)
-            image_paths.append(image_path)
-            list_feature.append(feature)
-            lsh.index(feature, extra_data=image_path)
-    pickle.dump(lsh, open('lsh.p', "wb"))
-    return lsh, image_paths, list_feature
-            
 if __name__ == '__main__':
     
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
--- utils_ai.py
+++ utils_ai.py
@@ -8,145 +8,14 @@ import sys
 import numpy
 
 import torch
+from PIL import Image
 from torch.optim.lr_scheduler import _LRScheduler
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+from conf import settings
 
 #from dataset import CIFAR100Train, CIFAR100Test
-
-def get_network(args, use_gpu=True):
-    """ return given network
-    """
-
-    if args.net == 'vgg16':
-        from models.vgg import vgg16_bn
-        net = vgg16_bn()
-    elif args.net == 'vgg13':
-        from models.vgg import vgg13_bn
-        net = vgg13_bn()
-    elif args.net == 'vgg11':
-        from models.vgg import vgg11_bn
-        net = vgg11_bn()
-    elif args.net == 'vgg19':
-        from models.vgg import vgg19_bn
-        net = vgg19_bn()
-    elif args.net == 'densenet121':
-        from models.densenet import densenet121
-        net = densenet121()
-    elif args.net == 'densenet161':
-        from models.densenet import densenet161
-        net = densenet161()
-    elif args.net == 'densenet169':
-        from models.densenet import densenet169
-        net = densenet169()
-    elif args.net == 'densenet201':
-        from models.densenet import densenet201
-        net = densenet201()
-    elif args.net == 'googlenet':
-        from models.googlenet import googlenet
-        net = googlenet()
-    elif args.net == 'inceptionv3':
-        from models.inceptionv3 import inceptionv3
-        net = inceptionv3()
-    elif args.net == 'inceptionv4':
-        from models.inceptionv4 import inceptionv4
-        net = inceptionv4()
-    elif args.net == 'inceptionresnetv2':
-        from models.inceptionv4 import inception_resnet_v2
-        net = inception_resnet_v2()
-    elif args.net == 'xception':
-        from models.xception import xception
-        net = xception()
-    elif args.net == 'resnet18':
-        from models.resnet import resnet18
-        net = resnet18()
-    elif args.net == 'resnet34':
-        from models.resnet import resnet34
-        net = resnet34()
-    elif args.net == 'resnet50':
-        from models.resnet import resnet50
-        net = resnet50()
-    elif args.net == 'resnet101':
-        from models.resnet import resnet101
-        net = resnet101()
-    elif args.net == 'resnet152':
-        from models.resnet import resnet152
-        net = resnet152()
-    elif args.net == 'preactresnet18':
-        from models.preactresnet import preactresnet18
-        net = preactresnet18()
-    elif args.net == 'preactresnet34':
-        from models.preactresnet import preactresnet34
-        net = preactresnet34()
-    elif args.net == 'preactresnet50':
-        from models.preactresnet import preactresnet50
-        net = preactresnet50()
-    elif args.net == 'preactresnet101':
-        from models.preactresnet import preactresnet101
-        net = preactresnet101()
-    elif args.net == 'preactresnet152':
-        from models.preactresnet import preactresnet152
-        net = preactresnet152()
-    elif args.net == 'resnext50':
-        from models.resnext import resnext50
-        net = resnext50()
-    elif args.net == 'resnext101':
-        from models.resnext import resnext101
-        net = resnext101()
-    elif args.net == 'resnext152':
-        from models.resnext import resnext152
-        net = resnext152()
-    elif args.net == 'shufflenet':
-        from models.shufflenet import shufflenet
-        net = shufflenet()
-    elif args.net == 'shufflenetv2':
-        from models.shufflenetv2 import shufflenetv2
-        net = shufflenetv2()
-    elif args.net == 'squeezenet':
-        from models.squeezenet import squeezenet
-        net = squeezenet()
-    elif args.net == 'mobilenet':
-        from models.mobilenet import mobilenet
-        net = mobilenet()
-    elif args.net == 'mobilenetv2':
-        from models.mobilenetv2 import mobilenetv2
-        net = mobilenetv2()
-    elif args.net == 'nasnet':
-        from models.nasnet import nasnet
-        net = nasnet()
-    elif args.net == 'attention56':
-        from models.attention import attention56
-        net = attention56()
-    elif args.net == 'attention92':
-        from models.attention import attention92
-        net = attention92()
-    elif args.net == 'seresnet18':
-        from models.senet import seresnet18
-        net = seresnet18()
-    elif args.net == 'seresnet34':
-        from models.senet import seresnet34 
-        net = seresnet34()
-    elif args.net == 'seresnet50':
-        from models.senet import seresnet50 
-        net = seresnet50()
-    elif args.net == 'seresnet101':
-        from models.senet import seresnet101 
-        net = seresnet101()
-    elif args.net == 'seresnet152':
-        from models.senet import seresnet152
-        net = seresnet152()
-
-    else:
-        print('the network name you have entered is not supported yet')
-        sys.exit()
-    
-    if use_gpu:
-        net = net.cuda()
-
-    return net
-
-
 def build_network(archi = 'squeezenet', use_gpu=True):
     """ return given network
     """
@@ -363,3 +232,51 @@ class WarmUpLR(_LRScheduler):
         rate to base_lr * m / total_iters
         """
         return [base_lr * self.last_epoch / (self.total_iters + 1e-8) for base_lr in self.base_lrs]
+    
+def get_feature_single_img(net, image_path):
+    device = torch.device("cuda")
+    image_transforms =  transforms.Compose([
+                        transforms.Resize((112, 112)),
+                        transforms.ToTensor(),
+                        transforms.Normalize(settings.CIFAR100_TRAIN_MEAN, 
+                                             settings.CIFAR100_TRAIN_STD)])
+
+    img = Image.open(image_path)
+    img_tensor = image_transforms(img)
+    model = net
+    if torch.cuda.is_available():
+        img_tensor = img_tensor.view(1, 3, 112, 112).cuda()
+    else:
+        img_tensor = img_tensor.view(1, 3, 112, 112)
+    with torch.no_grad():
+        model.eval()
+        # model ouputs log probabilities
+        out = model(img_tensor)  # <class 'torch.Tensor'>  torch.Size([1, 58])
+        ps = torch.exp(out) #  <class 'torch.Tensor'> torch.Size([1, 58])
+        feature = ps.cpu().numpy()[0]
+    return feature
+
+def create_feature(list_author, net):
+    global example_image_dir
+    list_feature = list()
+    image_paths = list()
+    ## Locality Sensitive Hashing
+    k = 10 # hash size
+    L = 5  # number of tables
+    d = 58 # Dimension of Feature vector
+    lsh = LSHash(hash_size=k, input_dim=d, num_hashtables=L)
+    for subfolder in list_author.keys():
+        subfolder_path = os.path.join(example_image_dir, subfolder)
+        count_items = len([name for name in os.listdir(subfolder_path) if os.path.isfile(os.path.join(subfolder_path, name))])
+        # print(subfolder)
+        sum_acc = 0
+        sum_confiden = 0
+
+        for img in os.listdir(subfolder_path):
+            image_path = os.path.join(subfolder_path, img)
+            author, confidence, feature = predict_author_single_img(net, image_path)
+            image_paths.append(image_path)
+            list_feature.append(feature)
+            lsh.index(feature, extra_data=image_path)
+    pickle.dump(lsh, open('lsh.p', "wb"))
+    return lsh, image_paths, list_feature
