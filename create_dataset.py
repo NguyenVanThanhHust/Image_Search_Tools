c2ecf2128fa3b197bac6795330b602601e1c57f3
NguyenVanThanhHust <NguyenVanThanhHust@gmail.com>
2020-02-11 18:58:01 +0700

    create training datasets

---
+++ create_dataset.py
@@ -0,0 +1,61 @@
+import logging
+import warnings
+warnings.filterwarnings('ignore')
+import argparse
+import os
+import sys
+import random
+
+from utils import copy_folder
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.WARNING)
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.ERROR)
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.CRITICAL)
+
+logging.info('Start program')
+handler = logging.FileHandler('infor.log')
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+def create_data_set(input_dataset_path, output_dataset_path):
+    # Randomly choose 10 classes from original datasets:
+    classes = next(os.walk(input_dataset_path))[1]
+    list_using_class = random.sample(classes, 10)
+    train_path = os.path.join(output_dataset_path, "train")
+    val_path = os.path.join(output_dataset_path, "val")
+    test_path = os.path.join(output_dataset_path, "test")
+    if not os.path.isdir(train_path):
+        os.mkdir(train_path)
+    if not os.path.isdir(val_path):
+        os.mkdir(val_path)
+    if not os.path.isdir(test_path):
+        os.mkdir(test_path)
+        
+    for each_class in list_using_class:
+        ori_path = os.path.join(input_dataset_path, each_class)
+        dst_path = output_dataset_path
+        copy_folder(each_class, ori_path, output_dataset_path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-idp', type=str, default= '../../datasets/101_ObjectCategories', help='input dataset path')
+    parser.add_argument('-odp', type=str, default= '../../datasets/Image_Search_Dataset', help='output dataset path')
+
+    args_dict = vars(parser.parse_args())
+    logger.info(args_dict)
+    input_dataset_path = args_dict['idp']
+    output_dataset_path = args_dict['odp']
+    if not os.path.isdir(input_dataset_path):
+        print("This is not a folder")
+        sys.exit()
+    if not os.path.isdir(output_dataset_path):
+        os.mkdir(output_dataset_path)
+    create_data_set(input_dataset_path, output_dataset_path)
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import re
+import os
+import random
+import shutil
 
 def get_class_name_from_string(image_path):
     """
@@ -15,4 +18,38 @@ def get_class_name_from_string(image_path):
     char_3_pos= [pos for pos, char in enumerate(image_name) if char == spe_char_3]
     class_name = image_name[0:char_3_pos[0]]
     return class_name
+
+def copy_folder(class_name, ori_path, dst_path):
+    train_path = os.path.join(dst_path, "train", class_name)
+    val_path = os.path.join(dst_path, "val", class_name)
+    test_path = os.path.join(dst_path, "test", class_name)
+    if not os.path.isdir(train_path):
+        os.mkdir(train_path)
+    if not os.path.isdir(val_path):
+        os.mkdir(val_path)
+    if not os.path.isdir(test_path):
+        os.mkdir(test_path)
+
+    list_file = next(os.walk(ori_path))[2]
+    train_files = random.sample(list_file, int(0.7 * len(list_file)))
+    val_test_files = [item for item in list_file if item not in train_files]
+    val_files = random.sample(list_file, int(0.5 * len(val_test_files)))
+    test_files = [item for item in val_test_files if item not in val_files]
+
+    # train/val/test/ = 0.7/0.15/0.15   
+    print("copy: ", len(list_file), " files from: ", ori_path, " to: ", dst_path)
+    for each_file in train_files:
+        src_file_path = os.path.join(ori_path, each_file)
+        dst_file_path = os.path.join(train_path, each_file)
+        shutil.copyfile(src_file_path, dst_file_path)
+    for each_file in val_files:
+        src_file_path = os.path.join(ori_path, each_file)
+        dst_file_path = os.path.join(val_path, each_file)
+        shutil.copyfile(src_file_path, dst_file_path)
+    for each_file in test_files:
+        src_file_path = os.path.join(ori_path, each_file)
+        dst_file_path = os.path.join(test_path, each_file)
+        shutil.copyfile(src_file_path, dst_file_path)
+
+
      
